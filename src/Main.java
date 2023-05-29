3eac6ecb78a2c1f63d7ae4d0f645e84d846c32c0
renanp96 <renanpa1996@gmail.com>
2023-05-29 16:55:05 -0300

    feature: criada estruturas basicas

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		Movie mv1 = new Movie();
 		mv1.setMovieName("Godzilla vs Kong");
+        mv1.setSynopsis("O terror de Toquio contra o solitario gorila da Ilha da Caveira");
         mv1.setReleaseYear(2022);
-		System.out.println(mv1.getMovieName());
+        mv1.setAvaliable(true);
+        mv1.setRating(8.1);
+
+		System.out.println(mv1.toString());
 	}
 }
